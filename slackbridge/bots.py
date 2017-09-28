fc7a71a0d215c3b1f5fbd9c9037c1b50047c6638
Benjamin Zhang <benzhang4@gmail.com>
2017-09-28 16:37:13 -0700

    Spelling is hard.

--- slackbridge/bots.py
+++ slackbridge/bots.py
@@ -80,7 +80,7 @@ class BridgeBot(IRCBot):
         if 'type' not in message:
             return
 
-        if message['type'] == 'prescence_change':
+        if message['type'] == 'presence_change':
             user_bot = self.users[message['user']]
             if message['presence'] == 'away':
                 user_bot.away()
